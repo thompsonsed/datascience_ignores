1e0c2e5d44d9908a9c8eec6e755addf11905f74d
thompsonsed <thompsonsed@gmail.com>
2020-03-02 10:46:12 +0000

    Don't write defaults again if they already exist in the gitignore file

--- create_gitignore.py
+++ create_gitignore.py
@@ -3,14 +3,43 @@ import pathlib
 import argparse
 
 def read_default_data() -> str:
+    """
+    Reads the default data from *default_ignores*
+
+    :return: the default ignore data
+    """
     default_ignores_path = pathlib.Path(pathlib.Path(__file__).parent, "default_ignores")
     if not default_ignores_path.exists():
         raise FileNotFoundError(f"Default ignores template file not found at {default_ignores_path.absolute()}.")
     with default_ignores_path.open("r") as d_path:
         return d_path.read()
 
+def check_existing_defaults(gitignore_path: pathlib.Path) -> bool:
+    """
+    Checks if the existing gitignore has the defaults already.
+
+    If the gitignore path does not exist, returns false.
+
+    Checks for the following pattern:
+
+    ############################
+    # Start of default ignores #
+    ############################
+
+    :param gitignore_path: the path of the existing .gitignore file.
+    :return: true if the defaults already exist.
+    """
+    if gitignore_path.exists():
+        expected_pattern = """############################
+        # Start of default ignores #
+        ############################"""
+        with gitignore_path.open("r") as git_file:
+            return expected_pattern in git_file.read()
+    return False
+
 
 def main():
+    """Runs the argument parsing and creation of the gitignore."""
     args = argparse.ArgumentParser(description="Create or add to an existing .gitignore with data science defaults.")
     args.add_argument(
         "-d",
@@ -37,10 +66,12 @@ def main():
         logging.info(f"Writing to destination .gitignore at {dest_path}.")
         if not dest_path.parent.exists():
             raise IOError(f"Directory does not exist at {dest_path.parent}")
-
-    with dest_path.open("a") as dest_path:
-        dest_path.write(read_default_data())
-    logging.info(f".gitignore defaults written to {dest_path}.")
+    if check_existing_defaults(dest_path):
+        logging.info(f"Defaults already exist in {dest_path}, exiting")
+    else:
+        with dest_path.open("a") as dest_path:
+            dest_path.write(read_default_data())
+        logging.info(f".gitignore defaults written to {dest_path}.")
 
 
 
